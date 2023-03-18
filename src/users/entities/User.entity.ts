b9dd0ce6906692d17b908d8882713ecc37bb176e
backupvs <dmitryholubev@gmail.com>
2023-03-18 16:41:51 +0200

    Fixed user role

--- src/users/entities/User.entity.ts
+++ src/users/entities/User.entity.ts
@@ -1,5 +1,4 @@
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
-import { Role } from "./role.enum";
 
 @Entity({ name: 'users' })
 export class User {
@@ -13,5 +12,5 @@ export class User {
     readonly password: string;
 
     @Column()
-    readonly role: Role;
+    readonly role: 'user' | 'admin';
 }
